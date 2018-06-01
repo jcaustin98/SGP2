1b259f097a422eb1c14f33374303346a27bc9dc9
jc <jeff@coleman-chaos.com>
2018-06-01 15:10:32 -0500

    wip for remote server

---
+++ SlotMachineSpinResults.php
@@ -0,0 +1,13 @@
+<?php
+/**
+Your code should validate the following request data: hash, coins won, coins bet, player ID
+
+.      Update the player data in MySQL if the data is valid
+.      Generate a JSON response with the following data:
+.      Player ID
+.      Name
+.      Credits
+.      Lifetime Spins
+.      Lifetime Average Return
+ */
+print_r($_REQUEST);
